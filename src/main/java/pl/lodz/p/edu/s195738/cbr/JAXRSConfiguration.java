7a557de0fb284f4175043fcb94820d3d70ba9edb
Mateusz Wiśniewski <siwypl@o2.pl>
2020-09-01 02:39:37 +0200

    Initial netbeans maven project

---
+++ src/main/java/pl/lodz/p/edu/s195738/cbr/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.edu.s195738.cbr;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
---
+++ src/main/java/pl/lodz/p/edu/s195738/cbr/resources/JavaEE8Resource.java
@@ -0,0 +1,20 @@
+package pl.lodz.p.edu.s195738.cbr.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("javaee8")
+public class JavaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
